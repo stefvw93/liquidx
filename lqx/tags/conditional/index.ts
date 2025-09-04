51166747f33442715efd98754ca311bf5a4c89c1
stefvw93 <stefvanwijchen@gmail.com>
2025-09-04 09:59:02 +0200

    first commit

---
+++ lqx/tags/conditional/index.ts
@@ -0,0 +1,54 @@
+import type { PropsWithChildren } from "../../jsx-runtime.ts";
+
+/**
+ * Renders a specific expression depending on the value of a specific variable.
+ */
+export function Case<T>(
+  { children, variable }: PropsWithChildren<{ variable: T }>,
+) {
+  return `{% case ${variable} %}${children}{% endcase %}`;
+}
+
+export function When<T extends unknown[]>(
+  { children, values }: PropsWithChildren<{ values: T }>,
+) {
+  return `{% when ${values.join(", ")} %}${children}`;
+}
+
+/**
+ * Allows you to specify a default expression to execute when no other condition is met.
+ * You can use the else tag with the following tags:
+ * - case
+ * - if
+ * - unless
+ */
+export function Else({ children }: PropsWithChildren) {
+  return `{% else %}${children}`;
+}
+
+/**
+ * Renders an expression if a specific condition is true.
+ */
+export function If(
+  { children, condition }: PropsWithChildren<{ condition: unknown }>,
+) {
+  return `{% if ${condition} %}${children}{% endif %}`;
+}
+
+/**
+ * You can use the elsif tag to check for multiple conditions.
+ */
+export function Elsif(
+  { children, condition }: PropsWithChildren<{ condition: unknown }>,
+) {
+  return `{% elsif ${condition} %}${children}`;
+}
+
+/**
+ * Renders an expression unless a specific condition is true.
+ */
+export function Unless(
+  { children, condition }: PropsWithChildren<{ condition: unknown }>,
+) {
+  return `{% unless ${condition} %}${children}{% endunless %}`;
+}
