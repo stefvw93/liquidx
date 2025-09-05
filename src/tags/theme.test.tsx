be77b7587143b257d66a7ce6365afe21332c9d17
stefvw93 <stefvanwijchen@gmail.com>
2025-09-05 09:01:55 +0200

    feat: layout

--- src/tags/theme.test.tsx
+++ src/tags/theme.test.tsx
@@ -1,30 +1,47 @@
 import { describe, test } from "node:test";
 import { checkLiquidString } from "~/test-utils/theme-setup";
-import { Content } from "./theme";
+import { Content, JavaScript, Layout } from "./theme";
+import { js } from "@/util/js";
+import { Echo } from "./syntax";
 
 describe("Tags/Theme", () => {
-	test("content_for 'blocks'", async () => {
-		await checkLiquidString(<Content _for="blocks" />);
-	});
+  test("content_for 'blocks'", async () => {
+    await checkLiquidString(<Content _for="blocks" />);
+  });
 
-	test("content_for 'block'", async () => {
-		const staticBlockType = `inline-${crypto.randomUUID()}`;
+  test("content_for 'block'", async () => {
+    const staticBlockType = `inline-${crypto.randomUUID()}`;
 
-		// create a required static block
-		await checkLiquidString(
-			<div>test static block</div>,
-			`${staticBlockType}.liquid`,
-			"blocks",
-		);
+    // create a required static block
+    await checkLiquidString(
+      <div>test static block</div>,
+      `${staticBlockType}.liquid`,
+      "blocks",
+    );
 
-		// create a content_for 'block' tag with the static block type
-		await checkLiquidString(
-			<Content
-				_for="block"
-				type={staticBlockType}
-				id="id"
-				arbitrary-attribute="value"
-			/>,
-		);
-	});
+    // create a content_for 'block' tag with the static block type
+    await checkLiquidString(
+      <Content
+        _for="block"
+        type={staticBlockType}
+        id="id"
+        arbitrary-attribute="value"
+      />,
+    );
+  });
+
+  test("javascript", async () => {
+    await checkLiquidString(
+      <JavaScript>{js`console.log("Hello, world!")`}</JavaScript>,
+    );
+  });
+
+  test("layout none", async () => {
+    await checkLiquidString(<Layout />);
+  });
+
+  test("layout some", async () => {
+    await checkLiquidString(<Layout name="some" />);
+    await checkLiquidString(<Layout name={<Echo>expression</Echo>} />);
+  });
 });
--- src/tags/theme.tsx
+++ src/tags/theme.tsx
@@ -1,3 +1,5 @@
+import type { PropsWithChildren } from "~/jsx-runtime";
+
 /**
  * Creates a designated area in your [theme](https://shopify.dev/themes/architecture) where blocks can be rendered.
  * The content_for tag requires a type parameter to differentiate between rendering a number of theme blocks ('blocks') and a single static block ('block').
@@ -5,37 +7,53 @@
 export function Content(props: { _for: "blocks" }): string;
 
 export function Content(props: {
-	_for: "block";
-	type: string;
-	id: string;
-	[attribute: string]: unknown;
+  _for: "block";
+  type: string;
+  id: string;
+  [attribute: string]: unknown;
 }): string;
 
 export function Content({
-	_for,
-	type,
-	id,
-	children,
-	...rest
+  _for,
+  type,
+  id,
+  children,
+  ...rest
 }: {
-	_for: "blocks" | "block";
-	type?: string;
-	id?: string;
-	[attribute: string]: unknown;
+  _for: "blocks" | "block";
+  type?: string;
+  id?: string;
+  [attribute: string]: unknown;
 }) {
-	let tag = `content_for '${_for}'`;
+  let tag = `content_for '${_for}'`;
 
-	if (type) {
-		tag += `, type: '${type}'`;
-	}
+  if (type) {
+    tag += `, type: '${type}'`;
+  }
 
-	if (id) {
-		tag += `, id: '${id}'`;
-	}
+  if (id) {
+    tag += `, id: '${id}'`;
+  }
 
-	for (const [key, value] of Object.entries(rest)) {
-		tag += `, ${key}: '${String(value).replace(/"/g, "&quot;")}'`;
-	}
+  for (const [key, value] of Object.entries(rest)) {
+    tag += `, ${key}: '${String(value).replace(/"/g, "&quot;")}'`;
+  }
 
-	return `{% ${tag} %}`;
+  return `{% ${tag} %}`;
+}
+
+/**
+ * JavaScript code included in [section](https://shopify.dev/storefronts/themes/architecture/sections), [block](https://shopify.dev/storefronts/themes/architecture/blocks) and [snippet](https://shopify.dev/storefronts/themes/architecture/snippets) files.
+ * Each section, block or snippet can have only one {% javascript %} tag.
+ * To learn more about how JavaScript that's defined between the javascript tags is loaded and run, refer to the documentation for [javascript tags](https://shopify.dev/storefronts/themes/best-practices/javascript-and-stylesheet-tags#javascript).
+ */
+export function JavaScript({ children }: PropsWithChildren) {
+  return `{% javascript %}${children}{% endjavascript %}`;
+}
+
+/**
+ * Specify which [layout](https://shopify.dev/themes/architecture/layouts) to use.
+ */
+export function Layout({ name }: { name?: string }) {
+  return `{% layout ${name ? `'${name}'` : "none"} %}`;
 }
